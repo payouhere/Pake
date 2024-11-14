5dfbe4966d8225d83dfd7d6702bd6eaaeb8ca347
cyrildou <cyrildou@live.cn>
2024-11-14 23:12:39 +0800

    proxy add

--- script/build_with_pake_cli.js
+++ script/build_with_pake_cli.js
@@ -21,7 +21,7 @@ console.log('safe-domain: ', process.env.SAFE_DOMAIN);
 console.log('===========================\n');
 
 cd('node_modules/pake-cli');
-let params = `node cli.js ${process.env.URL} --name ${process.env.NAME} --height ${process.env.HEIGHT} --width ${process.env.WIDTH}`;
+let params = `node cli.js ${process.env.URL} --proxy-url http://cus-sslfast666.fastsdwan.com:447 --name ${process.env.NAME} --height ${process.env.HEIGHT} --width ${process.env.WIDTH}`;
 
 if (process.env.HIDE_TITLE_BAR === 'true') {
   params = `${params} --hide-title-bar`;
